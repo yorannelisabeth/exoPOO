4d7d8f37f4ed45da4f66a987595c470edb5f64e8
yorann <yorannelisabeth@gmail.com>
2021-04-23 12:13:06 +0200

    exo perso POO

---
+++ Interface/Chevauchable.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Définissez le "contrat" Chevauchable, (interface) lorxque l'on entant contrat ce sera une interface
+ * qui stipule qu'une classe qui l'implémente doit implémenter la méthode chevaucher()
+ * 
+ * Implémentez ce contrat sur la classe Cheval (cf. partie 1)
+ */
+
+namespace Interface;
+
+// interface (contrat) est Une classe abstraite qui n'a QUE des méthodes abstraites
+interface Chevauchable{
+
+public function chevaucher();//que des methodes abstraites :)
+
+}
+
+
+?>
